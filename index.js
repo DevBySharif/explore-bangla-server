2068af6333687c8add2427fc76b2083befd65378
Sharif Obaidullah <sharifmdobaidullah@gmail.com>
2023-11-29 08:45:42 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     
     const packageCollection=client.db('exploreBanglaDB').collection('tourPackages')
     const guidesCollection=client.db('exploreBanglaDB').collection('tourGuides')
@@ -200,8 +200,8 @@ async function run() {
       res.send(result)
     })
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
